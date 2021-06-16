763bb7d8e856bdf2c5aa3c578671a7ceb724e025
ShaokangJiang <32856266+ShaokangJiang@users.noreply.github.com>
2021-06-16 10:46:19 +0800

    Basic format

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import Button from 'react-bootstrap/Button';
+import base64 from 'base-64';
+import urlencode from 'urlencode';
+import UIDGenerator from 'uid-generator';
+import paymentwall from 'paymentwall';
+
+function getPayment(uuid) {
+  const uidgen = new UIDGenerator();
+  // var a = await uidgen.generate();
+  // console.log(a);
+  return (<Button variant="primary" 
+    title={uuid} onClick={() => alert("aaaaa")} 
+  >{uuid}</Button>)
+}
 
 function App() {
   return (
@@ -9,6 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {getPayment(12)}
         <a
           className="App-link"
           href="https://reactjs.org"
